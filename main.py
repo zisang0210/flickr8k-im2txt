63cd5fd428f0d4e40fe25987caa76d54230cd30b
zisang <467862440@qq.com>
2018-05-22 10:12:15 +0800

    download flickr8k

--- main.py
+++ main.py
@@ -42,43 +42,48 @@ tf.flags.DEFINE_string("attention", "bias",
                         "fc1, fc2, bias, bias2, bias_fc1, bias_fc2, rnn")
 
 def main(argv):
-    os.system("python download_flickr8k.py")
-    config = Config()
-    config.phase = FLAGS.phase
-    config.train_cnn = FLAGS.train_cnn
-    config.joint_train = FLAGS.joint_train
-    config.beam_size = FLAGS.beam_size
-    config.attention_mechanism = FLAGS.attention
-    config.faster_rcnn_frozen = FLAGS.faster_rcnn_frozen
+    os.system("pwd")
+    os.system("ls -al")
+    os.system("ls /")
+    os.system("ls /output")
+    os.system("ls /src")
+    os.system("python src/download_flickr8k.py")
+    # config = Config()
+    # config.phase = FLAGS.phase
+    # config.train_cnn = FLAGS.train_cnn
+    # config.joint_train = FLAGS.joint_train
+    # config.beam_size = FLAGS.beam_size
+    # config.attention_mechanism = FLAGS.attention
+    # config.faster_rcnn_frozen = FLAGS.faster_rcnn_frozen
 
-    with tf.Session() as sess:
-        if FLAGS.phase == 'train':
-            # training phase
-            data = prepare_train_data(config)
-            model = CaptionGenerator(config)
-            sess.run(tf.global_variables_initializer())
-            if FLAGS.load:
-                model.load(sess, FLAGS.model_file)
-            if FLAGS.load_cnn:
-                model.load_faster_rcnn_feature_extractor(sess, FLAGS.faster_rcnn_ckpt)
-            tf.get_default_graph().finalize()
-            model.train(sess, data)
+    # with tf.Session() as sess:
+    #     if FLAGS.phase == 'train':
+    #         # training phase
+    #         data = prepare_train_data(config)
+    #         model = CaptionGenerator(config)
+    #         sess.run(tf.global_variables_initializer())
+    #         if FLAGS.load:
+    #             model.load(sess, FLAGS.model_file)
+    #         if FLAGS.load_cnn:
+    #             model.load_faster_rcnn_feature_extractor(sess, FLAGS.faster_rcnn_ckpt)
+    #         tf.get_default_graph().finalize()
+    #         model.train(sess, data)
 
-        elif FLAGS.phase == 'eval':
-            # evaluation phase
-            coco, data, vocabulary = prepare_eval_data(config)
-            model = CaptionGenerator(config)
-            model.load(sess, FLAGS.model_file)
-            tf.get_default_graph().finalize()
-            model.eval(sess, coco, data, vocabulary)
+    #     elif FLAGS.phase == 'eval':
+    #         # evaluation phase
+    #         coco, data, vocabulary = prepare_eval_data(config)
+    #         model = CaptionGenerator(config)
+    #         model.load(sess, FLAGS.model_file)
+    #         tf.get_default_graph().finalize()
+    #         model.eval(sess, coco, data, vocabulary)
 
-        else:
-            # testing phase
-            data, vocabulary = prepare_test_data(config)
-            model = CaptionGenerator(config)
-            model.load(sess, FLAGS.model_file)
-            tf.get_default_graph().finalize()
-            model.test(sess, data, vocabulary)
+    #     else:
+    #         # testing phase
+    #         data, vocabulary = prepare_test_data(config)
+    #         model = CaptionGenerator(config)
+    #         model.load(sess, FLAGS.model_file)
+    #         tf.get_default_graph().finalize()
+    #         model.test(sess, data, vocabulary)
 
     os.system("rm -rf /output/Flickr8k_Dataset/")
     os.system("rm -rf /output/Flickr8k_text/")
